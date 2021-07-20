e3301fc5d30c7167786dda1a209a9a8e2b2e61ff
ItzikEzra <itzikezra11@gmail.com>
2021-07-20 14:22:12 +0300

    Initial commit

---
+++ app/src/main/java/com/itzik_ezra/mediaplayerex1/Song.java
@@ -0,0 +1,54 @@
+package com.itzik_ezra.mediaplayerex1;
+
+public class Song {
+
+    String m_SongName;
+    String m_SongPerformer;
+    String m_PhotoPath;
+    String m_SongPath;
+
+
+    public void setM_SongName(String m_SongName) {
+        this.m_SongName = m_SongName;
+    }
+
+    public void setM_SongPerformer(String m_SongPerformer) {
+        this.m_SongPerformer = m_SongPerformer;
+    }
+
+    public void setM_PhotoPath(String m_PhotoPath) {
+        this.m_PhotoPath = m_PhotoPath;
+    }
+
+    public void setM_SongPath(String m_SongPath) {
+        this.m_SongPath = m_SongPath;
+    }
+
+
+    public String getM_SongName() {
+        return m_SongName;
+    }
+
+    public String getM_SongPerformer() {
+        return m_SongPerformer;
+    }
+
+    public String getM_PhotoPath() {
+        return m_PhotoPath;
+    }
+
+    public String getM_SongPath() {
+        return m_SongPath;
+    }
+
+    public Song(String m_SongName, String m_SongPerformer, String m_PhotoPath, String m_SongPath) {
+        this.m_SongName = m_SongName;
+        this.m_SongPerformer = m_SongPerformer;
+        this.m_PhotoPath = m_PhotoPath;
+        this.m_SongPath = m_SongPath;
+    }
+
+
+
+
+}
